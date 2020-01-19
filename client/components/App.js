e8eed9e7e05f4a833bb2c77e9a8ff8eb9b904804
solomiia <solomiiagulitashvili@gmail.com>
2020-01-19 17:54:25 +0200

    webpack for client created

---
+++ client/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/main.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+}
+
+export default App;
